cf21faa413405ed32f5178f5616a966552beac6a
PRAJITPRANAVK <pranavkarthi2t7@gmail.com>
2026-07-14 10:21:19 +0530

    Update

---
+++ selenium/test/membership_test.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import ExpectedConditions as EC
+
+driver = webdriver.chrome()
+
+driver.maximize_Window()
+
