bd625f42d6d093248a1b7ab2fbd18eba5858b58f
PRAJITPRANAVK <pranavkarthi2t7@gmail.com>
2026-07-14 19:59:34 +0530

    Update

---
+++ selenium/test/purchase_test.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import ExpectedCinditions as EC
+
+driver = webdriver.Chrome()
